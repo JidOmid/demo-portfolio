12f2af1c2ef611c6ba63f0a6d4c0f0364734de2b
Majid Omid <riothoodie@gmail.com>
2022-04-26 13:43:17 -0700

    Whole lotta stuff

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import { Container, Box, Heading } from "@chakra-ui/react"
+
+const Page = () => {
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        I am an aspiring front-end developer!
+      </Box>
+
+      <Box display={{md:'flex'}}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Majid Omid
+          </Heading>
+          <p>Gamer / Developer</p>
+        </Box>
+      </Box>
+    </Container>
+    
+  )
+}
+
+export default Page
